58a6f08ae26790c02cd58ba6ebf9ec33c0559325
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2026-03-02 09:03:22 +0530

    Moved index.html to root and added public apis

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+    COUNTRY_LIST_API : "https://restcountries.com/v3.1/all?fields=name,currencies,flag",
+    EXCHANGE_RATE_API_KEY : "e281d90f91ddf146cb4fc2da",
+    EXCHANGE_RATE_BASE_URL : "https://v6.exchangerate-api.com/v6/"
+}
